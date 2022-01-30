6184e58faf28226845893c9d2fe690173ef15b8a
IrinaAginskaya <aginska@gmail.com>
2022-01-30 16:30:08 +0200

    add api

--- src/services/api.js
+++ src/services/api.js
@@ -6,3 +6,57 @@ const KEY = '5eedd9576ab43a008551d36a9f8b0c8a';
 // https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
 // https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=<<api_key>>&language=en-US
 // https://api.themoviedb.org/3/movie/{movie_id}/reviews?api_key=<<api_key>>&language=en-US&page=1
+
+export function fetchPopularMovie() {
+  return;
+  fetch(`${BASE_URL}/trending/all/day?api_key=${KEY}`).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  });
+}
+
+export function fetchMovieByQuery(query) {
+  return;
+  fetch(
+    `${BASE_URL}/search/movie?api_key=${KEY}&query=${query}&language=en-US&page=1&include_adult=false`,
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  });
+}
+
+export function fetchMovieById(movieId) {
+  return;
+  fetch(`${BASE_URL}/movie/${movieId}?api_key=${KEY}&language=en-US`).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  });
+}
+
+export function fetchMovieCast(movieId) {
+  return;
+  fetch(`${BASE_URL}/movie/${movieId}/credits?api_key=${KEY}&language=en-US`).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  });
+}
+
+export function fetchMovieRewiew(movieId) {
+  return;
+  fetch(`${BASE_URL}/movie/${movieId}/reviews?api_key=${KEY}&language=en-US&page=1`).then(
+    response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    },
+  );
+}
